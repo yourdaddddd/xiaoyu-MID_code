24097072c5107b5030b48996cb2117c9cb3850c7
Xiaoyu Qi <xiaoyu1.qi@momenta.ai>
2023-10-16 11:10:56 +0800

    init

---
+++ dataset/dataset.py
@@ -0,0 +1,79 @@
+from torch.utils import data
+import numpy as np
+from .preprocessing import get_node_timestep_data
+
+
+class EnvironmentDataset(object):
+    def __init__(self, env, state, pred_state, node_freq_mult, scene_freq_mult, hyperparams, **kwargs):
+        self.env = env
+        self.state = state
+        self.pred_state = pred_state
+        self.hyperparams = hyperparams
+        self.max_ht = self.hyperparams['maximum_history_length']
+        self.max_ft = kwargs['min_future_timesteps']
+        self.node_type_datasets = list()
+        self._augment = False
+        for node_type in env.NodeType:
+            if node_type not in hyperparams['pred_state']:
+                continue
+            self.node_type_datasets.append(NodeTypeDataset(env, node_type, state, pred_state, node_freq_mult,
+                                                           scene_freq_mult, hyperparams, **kwargs))
+
+    @property
+    def augment(self):
+        return self._augment
+
+    @augment.setter
+    def augment(self, value):
+        self._augment = value
+        for node_type_dataset in self.node_type_datasets:
+            node_type_dataset.augment = value
+
+    def __iter__(self):
+        return iter(self.node_type_datasets)
+
+
+class NodeTypeDataset(data.Dataset):
+    def __init__(self, env, node_type, state, pred_state, node_freq_mult,
+                 scene_freq_mult, hyperparams, augment=False, **kwargs):
+     # 该类在初始化时需要传入环境、节点类型、状态、预测状态、节点频率乘数、场景频率乘数、超参数、是否增强和其他关键字参数
+        self.env = env
+        self.state = state
+        self.pred_state = pred_state
+        self.hyperparams = hyperparams
+        self.max_ht = self.hyperparams['maximum_history_length']
+        self.max_ft = kwargs['min_future_timesteps']
+
+        self.augment = augment# 是否进行数据增强
+
+        self.node_type = node_type# 节点类型
+        self.index = self.index_env(node_freq_mult, scene_freq_mult, **kwargs)
+        self.len = len(self.index)
+        self.edge_types = [edge_type for edge_type in env.get_edge_types() if edge_type[0] is node_type]
+
+    def index_env(self, node_freq_mult, scene_freq_mult, **kwargs):
+        index = list()
+        for scene in self.env.scenes:
+            present_node_dict = scene.present_nodes(np.arange(0, scene.timesteps), type=self.node_type, **kwargs)
+            for t, nodes in present_node_dict.items():
+                for node in nodes:
+                    index += [(scene, t, node)] *\
+                             (scene.frequency_multiplier if scene_freq_mult else 1) *\
+                             (node.frequency_multiplier if node_freq_mult else 1)
+                # 将 (场景, 时间步骤, 节点) 这样的元组加入到索引列表中
+                # 如果启用了场景频率乘数 scene_freq_mult，那么会将此元组重复加入场景的 frequency_multiplier 次
+                # 如果启用了节点频率乘数 node_freq_mult，那么会将此元组重复加入节点的 frequency_multiplier 次
+        return index
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, i):
+        (scene, t, node) = self.index[i]
+
+        if self.augment:
+            scene = scene.augment()
+            node = scene.get_node_by_id(node.id)
+
+        return get_node_timestep_data(self.env, scene, t, node, self.state, self.pred_state,
+                                      self.edge_types, self.max_ht, self.max_ft, self.hyperparams)
